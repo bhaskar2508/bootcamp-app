739083558db37823481c346bd9cbc3d1724429cd
bhaskar2508 <157706286+bhaskar2508@users.noreply.github.com>
2025-02-18 21:56:59 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Contact from './components/Contact';
+import Product from './components/Product';
+
+const routes = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>,
+      children:[{
+        path:'contact',
+        element:<Contact/>
+      },
+      {
+        path:'product',
+        element:<Product/>
+      }
+    ]
+  }
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello React</h1>
-
+  // <Home/> --- first this was imported later the second one  as 3.5 point i.e. impact outlet to dynamically render components 
+  <RouterProvider router={routes}/>
 );
 
 
